4a73815ed03bd8ec553f4454b36aa5966db2601f
Oseias <Oseias.YC@gmail.com>
2023-03-27 18:25:00 -0300

    Spring Security started

---
+++ src/main/java/com/ucsal/springlab/SecurityConfiguration.java
@@ -0,0 +1,56 @@
+package com.ucsal.springlab;
+
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration{
+
+    @Bean
+    public static BCryptPasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/static/**");
+    }
+    
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        return http
+                .authorizeHttpRequests(
+                    authorizeConfig -> {
+                        authorizeConfig.requestMatchers("/public").permitAll();
+                        authorizeConfig.anyRequest().authenticated();
+                    }
+                )
+                .formLogin(form -> form
+                        .loginPage("/login")
+                        .permitAll())
+        .build();
+    }
+    
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails professor = User.withUsername("professor")
+                .password(passwordEncoder().encode("professor"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(admin, professor);
+    }
+}
---
+++ src/main/java/com/ucsal/springlab/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.ucsal.springlab.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+}
