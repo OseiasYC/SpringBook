d1978b10a372f7761dcd29ff5efe7b3a0912fcd1
Oseias <Oseias.YC@gmail.com>
2023-03-27 19:37:53 -0300

    Bug fixes

--- src/main/java/com/ucsal/springlab/controllers/HomeController.java
+++ src/main/java/com/ucsal/springlab/controllers/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home() {
         return "home";
     }
