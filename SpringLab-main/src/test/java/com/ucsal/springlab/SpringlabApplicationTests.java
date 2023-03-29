ab5a008583edf82635790633a12240b3686c1d59
Marcos Pacheco <95505514+mcpmarcos@users.noreply.github.com>
2023-03-28 22:05:56 -0300

    Primeiro commit

---
+++ SpringLab-main/src/test/java/com/ucsal/springlab/SpringlabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ucsal.springlab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringlabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
