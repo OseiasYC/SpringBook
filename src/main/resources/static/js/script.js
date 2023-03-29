be98f3ecfc0b759c1589f0d3f7a5337f98a5813a
Oseias <Oseias.YC@gmail.com>
2023-03-29 12:46:49 -0300

    Home page bugs fixes

--- src/main/resources/static/js/script.js
+++ src/main/resources/static/js/script.js
@@ -1,9 +1,11 @@
 const button = document.querySelector("#button");
 const datepicker = document.querySelector("#datepicker");
 const message = document.querySelector("#message");
+const initial = document.querySelector("#initial-time");
+const final = document.querySelector("#final-time");
+var now = new Date();
 
 function getDate() {
-  var now = new Date();
   var year = now.getFullYear();
   var month = (now.getMonth() + 1).toString().padStart(2, "0");
   var day = now.getDate().toString().padStart(2, "0");
@@ -19,20 +21,6 @@ function getDate() {
       static: true,
       time_24hr: true,
     });
-
-    $("#button").click(function (event) {
-      event.preventDefault();
-      if (
-        $("#initial-time").val() == "" ||
-        $("#final-time").val() == "" ||
-        $("#inputLab").val() == null ||
-        $("#inputSubject").val() == null
-      ) {
-        $("#message").text("Please, check all the inputs");
-      } else {
-        $("form").submit();
-      }
-    });
   });
 
   function minTime() {
@@ -44,19 +32,33 @@ function getDate() {
   }
 }
 
-function verify() {
-  if (!datepicker.value) {
-    message.innerText = "Put date, first";
-    button.disabled = true;
-  }
-  if ($("#initial-time").val() != "") {
-    $("#final-time").min = now.getHours() + ":" + now.getMinutes(); // FIXME para resolver
-  }
+function finalTime() {
+  flatpickr("#final-time", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    minTime: initial.value,
+    static: true,
+    time_24hr: true,
+  });
 }
 
-function allow() {
-  message.innerText = "";
-  button.disabled = false;
-}
+//Checker
+$("#button").click(function (event) {
+  event.preventDefault();
+  if (
+    !initial.value ||
+    !final.value ||
+    !$("#inputLab").val() ||
+    !$("#inputSubject").val() ||
+    !datepicker.value
+  ) {
+    $("#message").text("Please, check all the inputs");
+  } else {
+    $("form").submit();
+  }
+});
 
 getDate();
+
+setTimeout(() => location.reload(), 300000);
