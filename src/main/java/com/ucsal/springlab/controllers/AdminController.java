375044599de41f242901feb9d17f1c0856a09886
Oseias <Oseias.YC@gmail.com>
2023-04-02 21:44:57 -0300

    Import main

--- src/main/java/com/ucsal/springlab/controllers/AdminController.java
+++ src/main/java/com/ucsal/springlab/controllers/AdminController.java
@@ -1,12 +1,17 @@
 package com.ucsal.springlab.controllers;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AdminController {
     @GetMapping("/admin")
-    public String admin() {
-        return "admin";
+    public ModelAndView admin(@AuthenticationPrincipal UserDetails user) {
+        ModelAndView admin = new ModelAndView("admin");
+        admin.addObject("user", user);
+        return admin;
     }
 }
---
+++ src/main/resources/static/js/script.js
@@ -0,0 +1,73 @@
+const button = document.querySelector("#button");
+const datepicker = document.querySelector("#datepicker");
+const message = document.querySelector("#message");
+const initial = document.querySelector("#initial-time");
+const final = document.querySelector("#final-time");
+
+function getDate() {
+  var now = new Date();
+  var year = now.getFullYear();
+  var month = (now.getMonth() + 1).toString().padStart(2, "0");
+  var day = now.getDate().toString().padStart(2, "0");
+  var nowDate = `${year}-${month}-${day}`;
+  datepicker.min = nowDate;
+
+  $(document).ready(function () {
+    flatpickr(".flatpickr-time", {
+      enableTime: true,
+      noCalendar: true,
+      dateFormat: "H:i",
+      minTime: minTime(),
+      static: true,
+      time_24hr: true,
+    });
+  });
+
+  function minTime() {
+    if (datepicker.value == null || datepicker.value != nowDate) {
+      return null;
+    } else {
+      return now.getHours() + ":" + now.getMinutes();
+    }
+  }
+}
+
+function finalTime() {
+  flatpickr("#final-time", {
+    enableTime: true,
+    noCalendar: true,
+    dateFormat: "H:i",
+    minTime: initial.value,
+    static: true,
+    time_24hr: true,
+  });
+}
+
+//Checker
+$("#button").click(function (event) {
+  event.preventDefault();
+  const selectedInitialTime = new Date(datepicker.value + "T" + initial.value + ":00");
+  const selectedFinalTime = new Date(datepicker.value + "T" + initial.value + ":00");
+  var now = new Date();
+
+  if (
+    !initial.value ||
+    !final.value ||
+    !$("#inputLab").val() ||
+    !$("#inputSubject").val() ||
+    !datepicker.value
+  ) {
+    $("#message").text("Please, check all the inputs");
+  } else {
+    if (initial.value || final.value) {
+      if (selectedInitialTime < now.getTime() || selectedFinalTime < now.getTime()) {
+        $("#message").text("You can't select past time");
+      } else {
+        $("form").submit();
+      }
+    }
+
+  }
+});
+
+getDate();
